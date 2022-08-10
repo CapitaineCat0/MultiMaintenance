fc2ac6cd935cf3307d657f31169907be5387d27c
CapitaineCat0 <33187305+CapitaineCat0@users.noreply.github.com>
2022-08-10 12:07:03 +0200

    Delete src/main directory

--- src/main/java/me/capitainecat0/multimaintenance/commands/CommandsListener.java
+++
@@ -1,9 +0,0 @@
-package me.capitainecat0.multimaintenance.commands;
-
-import me.capitainecat0.multimaintenance.MultiMaintenance;
-
-public class CommandsListener {
-    public void init(){
-        MultiMaintenance.instance().registerCommand(new MaintenanceToggle(), "maintenance");
-    }
-}
